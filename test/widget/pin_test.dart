70373ad674c3879123f0c50c3abd3dd8f10d0616
Lair Martes <lair.martes@skipthedishes.ca>
2020-06-17 21:46:16 -0500

    FEAT-PIN Introduce golden test for game start case

--- test/widget/pin_test.dart
+++ test/widget/pin_test.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hanoi_tower_control/hanoi_tower_control.dart';
+import 'package:hanoi_tower_game/widget/pin.dart' as ui_game;
 
 void main() {
-  testWidgets('When pin has receives 3 pins then it matches Golden', (WidgetTester tester)) async {
+  testWidgets('Golden test passes when starts game with 4 disks', (WidgetTester tester) async {
+    Game game = Game();
 
-  }
+    Progress progress = await game.start(4);
+
+    await tester.pumpWidget(MaterialApp(home: ui_game.Pin(progress.disksFirstPin())));
+
+    await expectLater(find.byType(ui_game.Pin), matchesGoldenFile('pin_start_with_4_disks.png'));
+  });
+  
+  testWidgets('Golden test passes when one disk is removed from a pin with 4 disks', (WidgetTester tester) async {
+
+    Game game = Game();
+
+    Progress progressStart = await game.start(4);
+
+    var uiPin = ui_game.Pin(progressStart.disksFirstPin());
+
+    game.grabFromFirstPin();
+
+  });
 }
