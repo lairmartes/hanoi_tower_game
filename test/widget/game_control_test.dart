e871f63f42c8ba15070da31ca1fe6d4dfdc7e5e9
Lair Martes <lair.martes@skipthedishes.ca>
2020-07-11 13:11:57 -0500

    FEAT-GAME Include test to check last progress updating

--- test/widget/game_control_test.dart
+++ test/widget/game_control_test.dart
@@ -3,22 +3,51 @@ import 'package:hanoi_tower_control/hanoi_tower_control.dart' as control;
 import 'package:hanoi_tower_game/widget/game.dart';
 
 void main() {
-  test('When game start with 3 disks, then last progress is disk 3, 0, 0 and grabbed is null', () async {
+  test('When game start with 3 disks, then last progress is equal to test game progress', () async {
 
     control.Game testControlGame = control.Game();
     GameController gameController = GameController();
 
-    testControlGame.start(3).then((expected) => {
-      gameController.startGame(3).then((_) {
-        control.Progress actual = gameController.lastProgress;
-        var actualData = {'pin1Disks':actual.disksFirstPin().disks.length };
-        var expectedData = {'pin1Disks':expected.disksFirstPin().disks.length };
+    control.Progress expected = await testControlGame.start(3);
 
-        expect(actualData, expectedData);
-      })
+    await gameController.startGame(3);
 
-    }).catchError((onError) {
-      fail('It is not possible to test due an error when starting test control game: $onError');
-    });
+    control.Progress actual = gameController.lastProgress;
+
+    expect(actual, expected);
+  });
+
+  test('When disk is grabbed, then las progress is equal to the test game progress', () async {
+
+    control.Game testControlGame = control.Game();
+    GameController gameController = GameController();
+
+    await testControlGame.start(3);
+    control.Progress expected = await testControlGame.grabFromFirstPin();
+
+    await gameController.startGame(3);
+    await gameController.moveDisk(1);
+
+    control.Progress actual = gameController.lastProgress;
+
+    expect(actual, expected);
+  });
+
+  test('When moved from disk 1 to disk 2, then las progress is equal to the test game progress', () async {
+
+    control.Game testControlGame = control.Game();
+    GameController gameController = GameController();
+
+    await testControlGame.start(3);
+    var grabDisk = await testControlGame.grabFromFirstPin();
+    var expected = await testControlGame.dropDiskInSecondPin(grabDisk.diskGrabbed);
+
+    await gameController.startGame(3);
+    await gameController.moveDisk(1);
+    await gameController.moveDisk(2);
+
+    control.Progress actual = gameController.lastProgress;
+
+    expect(actual, expected);
   });
 }
